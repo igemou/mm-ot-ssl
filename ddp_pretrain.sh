973307bc03d4d35336474b5c3f556d5c040f6cce
joobumjin <bumjin.joo@gmail.com>
2025-11-12 16:51:46 -0500

    adding slurm bash file

---
+++ ddp_pretrain.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu --gres=gpu:1 --output=mae_ddp.out
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 02:00:00
+#SBATCH --mem=8g
+
+export MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1) 
+export MASTER_PORT=12345 
+
+# Load a CUDA module
+module load cuda
+module load miniconda3/23.11.0s
+source /gpfs/runtime/opt/miniconda/4.12.0/etc/profile.d/conda.sh
+
+conda activate mae
+
+# Run program
+cd /users/bjoo2/code/mae_cs2952x
+
+echo "Pretraining DDP"
+srun torchrun \
+        --nnodes=2 \
+        --nproc_per_node=1 \
+        --rdzv_id=100 \
+        --rdzv_backend=c10d \
+        --rdzv_endpoint=$MASTER_ADDR:29400 \
+         train_ddp.py --batch_size 64 --epochs 50
+
+
